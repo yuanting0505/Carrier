14fc1b60400159ed6e077e2830fe5f5d2df9925c
yuanting0505 <yuantingisme@gmail.com>
2013-12-16 21:31:06 +0800

    a sample form done

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Carrier::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
@@ -53,4 +53,7 @@ Carrier::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get 'senders/new'=>'senders#new'
+  resources :cards
 end
